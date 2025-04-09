c896c827f33e563808840394a4bb37bbb3a086ff
ramonespier <ramon.coelhomelo@outlook.com>
2025-04-09 11:19:51 -0300

    new: dropdown component

---
+++ app/src/app/components/Dropdown/dropdown.jsx
@@ -0,0 +1,104 @@
+"use client"
+
+import {
+    FiEdit,
+    FiChevronDown,
+    FiTrash,
+    FiShare,
+    FiPlusSquare,
+  } from "react-icons/fi";
+  import { motion } from "framer-motion";
+  import { Dispatch, SetStateAction, useState } from "react";
+  import { IconType } from "react-icons";
+  
+  const Dropdown = () => {
+    const [open, setOpen] = useState(false);
+  
+    return (
+      <div className="flex items-center justify-center">
+        <motion.div animate={open ? "open" : "closed"} className="relative">
+          <button
+            onClick={() => setOpen((pv) => !pv)}
+            className="flex items-center justify-center gap-2 px-2 py-2 rounded-md border border-white/50 text-indigo-50 bg-rose-950/50 hover:bg-rose-950 transition-colors w-12"
+          >
+            <motion.span variants={iconVariants}>
+              <FiChevronDown />
+            </motion.span>
+          </button>
+  
+          <motion.ul
+            initial={wrapperVariants.closed}
+            variants={wrapperVariants}
+            style={{ originY: "top", translateX: "-50%" }}
+            className="flex flex-col gap-2 p-2 rounded-lg bg-white shadow-xl absolute top-[120%] left-[50%] w-48 overflow-hidden"
+          >
+            <Option setOpen={setOpen} Icon={FiEdit} text="Edit" />
+            <Option setOpen={setOpen} Icon={FiPlusSquare} text="Duplicate" />
+            <Option setOpen={setOpen} Icon={FiShare} text="Share" />
+            <Option setOpen={setOpen} Icon={FiTrash} text="Remove" />
+          </motion.ul>
+        </motion.div>
+      </div>
+    );
+  };
+  
+  const Option = ({ text, Icon, setOpen }) => {
+    return (
+      <motion.li
+        variants={itemVariants}
+        onClick={() => setOpen(false)}
+        className="flex items-center gap-2 w-full p-2 text-xs font-medium whitespace-nowrap rounded-md hover:bg-indigo-100 text-slate-700 hover:text-indigo-500 transition-colors cursor-pointer"
+      >
+        <motion.span variants={actionIconVariants}>
+          <Icon />
+        </motion.span>
+        <span>{text}</span>
+      </motion.li>
+    );
+  };
+  
+  export default Dropdown;
+  
+  const wrapperVariants = {
+    open: {
+      scaleY: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.1,
+      },
+    },
+    closed: {
+      scaleY: 0,
+      transition: {
+        when: "afterChildren",
+        staggerChildren: 0.1,
+      },
+    },
+  };
+  
+  const iconVariants = {
+    open: { rotate: 180 },
+    closed: { rotate: 0 },
+  };
+  
+  const itemVariants = {
+    open: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        when: "beforeChildren",
+      },
+    },
+    closed: {
+      opacity: 0,
+      y: -15,
+      transition: {
+        when: "afterChildren",
+      },
+    },
+  };
+  
+  const actionIconVariants = {
+    open: { scale: 1, y: 0 },
+    closed: { scale: 0, y: -7 },
+  };
