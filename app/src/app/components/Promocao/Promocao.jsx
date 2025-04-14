d4b188a5b5f9f7cd6a17e8bad41c2ad3c14e1aa5
ramonespier <ramon.coelhomelo@outlook.com>
2025-04-14 10:25:43 -0300

    style: responsividade parcial

--- app/src/app/components/Promocao/Promocao.jsx
+++ app/src/app/components/Promocao/Promocao.jsx
@@ -10,30 +10,56 @@ export default function Promocao() {
         <>
 
             {/* div pai */}
-            <div className="flex m-5 items-center">
+            <div className="grid justify-items-center
+                            grid-rows-1 grid-cols-1
+                            lg:grid-cols-2 
+                            md:grid-cols-1 
+                            m-5">
 
-                <div className="flex flex-col gap-2.5">
-                    <h1 className="font-extrabold text-3xl">Melhore sua experiência com o premium!</h1>
-                    <p className="break-words w-200 font-bold text-gray-300">Assista a animes e séries sem anúncios e aproveite a entrega rápida e gratuita em diversos produtos. Não perca essa oportunidade de mergulhar no universo dos animes com a melhor qualidade! Aproveite também a entrega rápida e gratuita em diversos produtos."
+                <div className="flex flex-col items-center
+                                row-start-1 col-start-1 
+                                lg:row-start-1 lg:col-start-1 
+                                md:row-start-1 md:col-start-1 
+                                gap-2.5 m-5">
+
+                    <h1 className="px-3 text- font-extrabold text-3xl w-svw md:w-md">Melhore sua experiência com o premium!</h1>
+                    <p className="px-3 break-words text-justify font-bold text-gray-300 w-svw md:w-md">Assista a animes e séries sem anúncios e aproveite a entrega rápida e gratuita em diversos produtos. Não perca essa oportunidade de mergulhar no universo dos animes com a melhor qualidade! Aproveite também a entrega rápida e gratuita em diversos produtos."
 
                         Agora está mais simples, mantendo o foco nas vantagens de assistir sem anúncios e na entrega.</p>
 
-                        {/* botões */}
-                    <div className="w-90 m-5">
-                    <button type="button" className="font-bold uppercase p-5 cursor-pointer bg-rose-900 w-90     rounded-xl hover:text-black hover:opacity-85 duration-300">Já é Neko? Faça login</button>
-                    <p className="font-extrabold text-center">----- ou -----</p>
-                    <button type="button" className="uppercase p-5 cursor-pointer  bg-rose-900 rounded-xl hover:opacity-85 hover:text-black duration-300 w-90    font-bold">Experimente 7 dias grátis</button>
+                    {/* botões */}
+                    <div className="m-5 flex flex-col items-center">
+                        <button type="button" className="font-bold uppercase p-5 cursor-pointer bg-rose-900 w-90 rounded-xl hover:text-black hover:opacity-85 duration-300">Já é Neko? Faça login</button>
+                        <p className="font-thin text-center">————— ou —————</p>
+                        <button type="button" className="uppercase p-5 cursor-pointer  bg-rose-900 rounded-xl hover:opacity-85 hover:text-black duration-300 w-90    font-bold">Experimente 7 dias grátis</button>
                     </div>
 
                 </div>
 
-                <div className="flex justify-center mt-5"></div>
-                <div className="animes flex w-[60%] h-[450px]">
+                <div className="animes hidden lg:flex p-5 lg:row-start-1 lg:col-start-2 lg:w-[60%] lg:h-[450px]">
                     {animes.map((anime) => (
-                        <img key={anime.id} src={anime.img} className="w-0 grow-100 object-cover duration-300 ease-out opacity-40 rounded-2xl m-[1%] hover:w-[117px] hover:opacity-100 hover:contrats-[120%]" ></img>
+                        <img key={anime.id} src={anime.img}
+                            className="w-18 grow-100 object-cover 
+                        duration-300 ease-out md:opacity-40
+                        rounded-2xl m-[1%] hover:w-[140px] 
+                        hover:opacity-100 hover:contrats-[120%]" ></img>
                     ))}
                 </div>
             </div>
+
+            <div className="animes lg:hidden p-5 w-full h-auto row-start-2 col-start-1">
+                <div className="flex flex-wrap justify-center">
+                    {animes.map((anime) => (
+                        <img
+                            key={anime.id}
+                            src={anime.img}
+                            alt=""
+                            className="w-[150px] sm:w-[30%] md:w-[25%] lg:w-[15%] h-[150px] object-cover duration-300 ease-out opacity-90 rounded-2xl m-2"
+                        />
+                    ))}
+                </div>
+            </div>
+
             <p className="text-rose-600 font-extrabold text-[20px] flex justify-center">Comparar todos os planos abaixo &darr;</p>
 
             {/* fim div pai */}
