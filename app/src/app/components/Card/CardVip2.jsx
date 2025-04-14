f7acec155a674a63eb9476965aa630782720bd97
davithekid <byjdrk.27@gmail.com>
2025-04-14 11:18:00 -0300

    fix (centralizar card chatp)

--- app/src/app/components/Card/CardVip2.jsx
+++ app/src/app/components/Card/CardVip2.jsx
@@ -4,13 +4,16 @@ export default function CardVip1() {
     
       <div className="card w-85 lg:w-[400] bg-rose-900 text-white p-8 rounded-xl border-2 mt-8">
         <div className="list flex flex-col items-center gap-5">
-          <div className="titulo font-extrabold text-3xl"><h2>FAN</h2></div>
-          <div className="price text-3xl font-extrabold">R$ 10,99/mês</div>
+          <div className="titulo font-extrabold text-3xl"><h2>MEGAFAN</h2></div>
+          <div className="price text-3xl font-extrabold">R$ 19,99/mês</div>
           <div className="button text-[16px] font-bold bg-rose-700 hover:bg-rose-950 duration-200 ease-in rounded-lg p-2"><button type="button">Comece um teste grátis de 7 dias</button></div>
           <div className="detalhes font-mono"><p>Assista a todo o acervo da Crunchyroll, sem propagandas, com novos episódios logo após o lançamento no Japão.*</p></div>
           <div className="flex flex-col">
             <span className="uppercase text-[12px]">——— plus ———</span>
             <span className="text-[13px]">✔ Assista em até 1 tela</span>
+            <span className="text-[13px]">✔ Offline viewing</span>
+            <span className="text-[13px]">✔ Acesse a Neko Game Vault, um catálogo de jogos gratuitos
+            </span>
           </div>
         </div>
       </div>
