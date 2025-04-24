d1bb5c1502c3406f29e7edd7cf9d34fea0eeb854
ramonespier <ramon.coelhomelo@outlook.com>
2025-04-23 21:07:41 -0300

    feat: search bar / adicionar imagens e links de catalogo

--- app/src/app/components/Card-catalogo/Card-catalogo.jsx
+++
@@ -1,8 +0,0 @@
-import catalogo from '../../catalogo.json'
-
-export default function CardCatalogo() {
-catalogo.map((dados) => (
-        <div key={dados.id}
-        className='w-10 h-10 bg-amber-500'>aaa</div>
-    ))
-}
