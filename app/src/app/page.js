8e677c99dba107a086ecf255015361b4d1f5aeab
ramonespier <ramon.coelhomelo@outlook.com>
2025-04-25 10:40:28 -0300

    chore: ideia anterior (remoção de aleatorização)

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,63 +1,45 @@
 "use client"
 
 import { catalogo } from "./catalogo.js";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import './page.css'
-import Link from 'next/link'
 
 export default function Catalogo() {
 
-  const [search, setSearch] = useState("")
-  const [shuffledAnimes, setShuffledAnimes] = useState([]);
-  const [moreItems, setMoreItems] = useState([])
+    const [search, setSearch] = useState("")
 
-  useEffect(() => {
-    const shuffled = [...animes].sort(() => 0.5 - Math.random())
-    setShuffledAnimes(shuffled)
-  }, [])
-  // console.log(search)
+    // console.log(search)
 
-  const searchLowerCase = search.toLowerCase()
-  animes = shuffledAnimes
-    .filter(anime => anime.titulo.toLowerCase().includes(searchLowerCase))
-    .slice(0, moreItems);
+    const searchLowerCase = search.toLowerCase()
 
-  const loadMore = () => {
-    setMoreItems(prev => prev + 8);
-  };
+    const animes = catalogo.filter(anime => anime.titulo.toLowerCase().includes(searchLowerCase))
 
-  const animes = catalogo.filter(anime => anime.titulo.toLowerCase().includes(searchLowerCase))
+    return (
+        <div>
+            <div className="w-full flex flex-col p-6">
 
-
-
-  return (
-    <div>
-      <div className="w-full flex flex-col p-6">
-
-        <div className="flex lg:justify-end justify-center">
-          <div className="flex items-center border gap-2 rounded-lg w-96 p-0.5 bg-blue-50 text-black">
-            <label htmlFor="search">🔎</label>
-            <input id="search" type="search" placeholder="Black Clover . . ." value={search} onChange={(e) => setSearch(e.target.value)}
-              className="
+                <div className="flex items-center border gap-2 rounded-lg w-96 p-0.5 bg-blue-50 text-black">
+                    <label htmlFor="search">🔎</label>
+                    <input id="search" type="search" placeholder="Black Clover . . ." value={search} onChange={(e) => setSearch(e.target.value)}
+                        className="
                 w-full
                 border-none
                 appearance-none
                 p-1
                 px-3
                 "/>
-          </div>
-        </div>
+                </div>
 
-        <ul className="
+                <ul className="
                 m-10
                 grid 
                 grid-cols-[repeat(auto-fill,minmax(275px,1fr))] 
                 justify-items-center    
                 gap-8">
 
-          {animes.map(anime => (
-            <li key={anime.id}
-              className="
+                    {animes.map(anime => (
+                        <li key={anime.id}
+                            className="
                         transition
                         border-2
                         max-w-[300px] 
@@ -70,47 +52,37 @@ export default function Catalogo() {
                         hover:scale-105
                         cursor-pointer
                         ">
-              <Link href={`/filmes/${anime.id}`}>
-                <div className="w-full h-full p-5">
-                  <div className="
+                            <a href={`/filmes/${anime.id}`}>
+                            <div className="w-full h-full p-5">
+
+
+
+                            <div className="
                             flex 
                             flex-col 
                             gap-4">
 
-                    <h3 className="
+                                <h3 className="
                                 text-center 
                                 text-2xl 
                                 font-extrabold">
-                      {anime.titulo}
-                    </h3>
+                                    {anime.titulo}
+                                </h3>
 
-                    <img className="border-2 border-neutral-200/70"
-                      src={anime.foto} />
+                                <img className="border-2 border-neutral-200/70"
+                                    src={anime.foto} />
 
-                    <p className="text-justify text-lg text-">{anime.descricao}</p>
-                  </div>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-
-        {/* Botão para carregar mais itens */}
-      {filteredAnimes.length > 0 && filteredAnimes.length % 8 === 0 && (
-        <div className="flex justify-center">
-          <button 
-            onClick={loadMore}
-            className="px-6 py-2 bg-amber-500 text-white rounded-lg hover:bg-amber-600 transition-colors"
-          >
-            Carregar Mais
-          </button>
+                                <p className="text-justify text-lg text-">{anime.descricao}</p>
+                            </div>
+                                    </div>
+                                    </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
-      )}  
-      </div>
-    </div>
 
-  )
+    )
 
 }
-
 
